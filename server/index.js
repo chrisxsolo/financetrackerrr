440b5e5697c5dcfff180c833e8e8107865832588
Chris Solorzano <chrisxsolo177@gmail.com>
2023-06-15 18:07:25 -0700

    deployment

--- server/index.js
+++ server/index.js
@@ -8,27 +8,7 @@ const cors = require("cors");
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(
-  "mongodb+srv://user123:Password123Tech@cluster0.j7fql.mongodb.net/merntutorial?retryWrites=true&w=majority"
-);
 
-app.get("/getUsers", (req, res) => {
-  UserModel.find({}, (err, result) => {
-    if (err) {
-      res.json(err);
-    } else {
-      res.json(result);
-    }
-  });
-});
-
-app.post("/createUser", async (req, res) => {
-  const user = req.body;
-  const newUser = new UserModel(user);
-  await newUser.save();
-
-  res.json(user);
-});
 
 app.listen(3001, () => {
   console.log("SERVER RUNS PERFECTLY!");
